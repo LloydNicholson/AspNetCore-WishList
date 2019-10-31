3601d16eedbde0c4236d75e91dc552b322767a5c
Lloyd Nicholson <lloydn@twizza.co.za>
2019-10-31 13:01:00 +0200

    Completed module

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        
+        [HttpGet]
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
